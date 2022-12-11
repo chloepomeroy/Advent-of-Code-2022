30d7246b7960add4e2636e585ac4ab7267751887
chloepomeroy <68558959+chloepomeroy@users.noreply.github.com>
2022-12-11 16:26:47 -0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[48]:
+
+
+cal = open("day1.txt", "r")
+data = cal.read()
+
+
+# In[52]:
+
+
+sums=[]
+for elf in data.split("\n\n"):
+    sums.append(sum([int(snack_calories) for snack_calories in elf.split("\n")]))
+
+
+# In[54]:
+
+
+#get the highest calorie sum
+print(f"part 1 answer: {max(sums)}")
+
+
+# In[63]:
+
+
+sums.sort(reverse=True)
+
+# get sum of top three elves
+print(f"part 2 answer: {sum(sums[0:3])}")
+
