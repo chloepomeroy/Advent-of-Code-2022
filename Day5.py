623eb02264e78ff13223923079dff16882d7e6c8
chloepomeroy <68558959+chloepomeroy@users.noreply.github.com>
2022-12-11 18:02:49 -0500

    Add files via upload

---
+++ Day5.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+import pandas as pd
+
+
+# In[32]:
+
+
+moves = pd.read_csv("day5moves.txt", header=None, names=["moves"])
+
+
+# In[33]:
+
+
+moves["Number of crates"] = moves["moves"].str.extract('(\d+)')
+moves["From"] = moves["moves"].str.extract('\d.*(\d+).*\d')
+moves["To"] = moves["moves"].str.extract('\d.*(\d+)')
+
+
+# In[42]:
+
+
+positions = ['NDMQBPZ', 'CLZQMDHV', 'QHRDVFZG', 'HGDFN', 'NFQ', 'DQVZFBT', 'QMTZDVSH', 'MGFPNQ', 'BWRM']
+
+for i in range(moves.shape[0]):
+    fr = int(moves["From"][i])-1
+    to = int(moves["To"][i])-1
+    for j in range(int(moves["Number of crates"][i])):
+        positions[fr], positions[to] = positions[fr][:-1], positions[to]+positions[fr][-1]
+
+print(f"part 1 answer: {''.join([c[-1] for c in positions])}")
+
+
+# In[54]:
+
+
+positions2 = ['NDMQBPZ', 'CLZQMDHV', 'QHRDVFZG', 'HGDFN', 'NFQ', 'DQVZFBT', 'QMTZDVSH', 'MGFPNQ', 'BWRM']
+
+for i in range(moves.shape[0]):
+    fr = int(moves["From"][i])-1
+    to = int(moves["To"][i])-1
+    num = int(moves["Number of crates"][i])
+    positions2[fr], positions2[to] = positions2[fr][:-num], positions2[to]+positions2[fr][-num:]
+
+print(f"part 2 answer: {''.join([c[-1] for c in positions2])}")
+
+
+# In[ ]:
+
+
+
+
